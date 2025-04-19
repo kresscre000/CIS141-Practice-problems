b09a33681887f6385236ab0bf90dece8910d9cb8
Clay I Kresser <104952050+kresscre000@users.noreply.github.com>
2025-04-19 01:46:49 -0700

    Update problem3.py

--- MOD_2/problem3.py
+++ MOD_2/problem3.py
@@ -1,6 +1,6 @@
-side1 = float(input("What is the length of side 1?: "))
-side2 = float(input("What is the length of side 2?: "))
-side3 = float(input("What is the length of side 3?: "))
+side1 = float(input("What is the length of side 1?:"))
+side2 = float(input("What is the length of side 2?:"))
+side3 = float(input("What is the length of side 3?:"))
 s = (side1 + side2 + side3) / 2
 area = (s * (s - side1) * (s - side2) * (s - side3)) ** 0.5
 print("The area of the triangle is:", area,)
