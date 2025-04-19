3c1e573ec73460e3408370ac40c35b82003b0d5f
Clay I Kresser <104952050+kresscre000@users.noreply.github.com>
2025-04-19 01:35:17 -0700

    Update problem2.py

--- MOD_2/problem2.py
+++ MOD_2/problem2.py
@@ -1,1 +1,11 @@
-
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+print("Results:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
