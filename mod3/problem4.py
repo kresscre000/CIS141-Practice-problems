a63de728d02baa5a233035430cbb5fedd9d9431a
Clay I Kresser <104952050+kresscre000@users.noreply.github.com>
2025-04-27 10:37:13 -0700

    Update problem4.py

--- mod3/problem4.py
+++ mod3/problem4.py
@@ -5,4 +5,4 @@ index1 = int(index1)
 index2 = int(index2)
 sliced = word[index1:index2]
 print(sliced)
-#I had to get help with this one mainly because I just dont fully understand slices still, is there any extra reading or practice problems I can do in my free time to help better understand slices?
+#I had to get help with this one mainly because I just dont fully understand these concepts still, is there any extra reading or practice problems I can do in my free time to help better understan?
