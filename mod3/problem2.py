7fa236021966c95d8eddc6bf241e737560e62d75
Clay I Kresser <104952050+kresscre000@users.noreply.github.com>
2025-04-27 10:10:01 -0700

    Update problem2.py

--- mod3/problem2.py
+++ mod3/problem2.py
@@ -1,1 +1,5 @@
-
+name = input("What is your name?")
+age = input("How old are you?")
+age = int(age)
+age2 = age + 1
+print("Hello, " + name + ". You are " + str(age) + " years old. Next year, you will be " + str(age2) + " years old!")
