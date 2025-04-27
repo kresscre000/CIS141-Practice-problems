36a5c8b92945d37874274770b44306616f2f1c4b
Clay I Kresser <104952050+kresscre000@users.noreply.github.com>
2025-04-27 10:34:14 -0700

    Update problem4.py

--- mod3/problem4.py
+++ mod3/problem4.py
@@ -1,1 +1,8 @@
-
+word = input("Please give us a word: ")
+index1 = input("What is the first index?: ")
+index2 = input("What is the last index?: ")
+index1 = int(index1)
+index2 = int(index2)
+sliced = word[index1:index2]
+print(sliced)
+#I had to get help with this one mainly because I just dont fully understand slices still, is there any extra reading or practice problems I can do in my free time to help better understand slices?
